b6c90f31752ee754c0d6f8d7f8f62f7c54545502
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-11 04:57:59 +0900

    Update lunapottery.js

--- lunapottery.js
+++ lunapottery.js
@@ -35,10 +35,10 @@ function initialDate()
 
 function seasonColor(c)
 {
-	var c_=cycle(c,59+45,365);
-	if(c<92) return lerpColor(color(188,206,172), color(172,206,204), map(c_,0,92,0,1));
-	else if(c<184) return lerpColor(color(172,206,204), color(196,182,157), map(c_,92,184,0,1));
-	else if(c<275) return lerpColor(color(196,182,157), color(171,177,182), map(c_,184,275,0,1));
+	var c_=cycle(c,-59-45,365);
+	if(c_<92) return lerpColor(color(188,206,172), color(172,206,204), map(c_,0,92,0,1));
+	else if(c_<184) return lerpColor(color(172,206,204), color(196,182,157), map(c_,92,184,0,1));
+	else if(c_<275) return lerpColor(color(196,182,157), color(171,177,182), map(c_,184,275,0,1));
 	else return lerpColor(color(171,177,182), color(188,206,172), map(c_,275,365,0,1));
 }
 
