08a2ac5ad811716da040b2d69e4b5fa7eb5638b7
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-11 18:29:20 +0900

    Update lunapottery.js

--- lunapottery.js
+++ lunapottery.js
@@ -95,14 +95,15 @@ function draw()
 	sansuTexture.rect(0,0,sansuTexture.width,sansuTexture.height);
 	
 	//rendering floor-pulse
-	floorTexture.shader(floorShader);
+//	floorTexture.shader(floorShader);
 	
 	floorShader.setUniform("uResolution", [floorTexture.width, floorTexture.height]);
 	floorShader.setUniform("uTime", millis() / 1000.0);
 	floorShader.setUniform("inCol", seasonCol[0]._array);
-	floorTexture.background(0);
+//	floorTexture.background(0);
+	floorTexture.fill(128);
 	// passing the shaderTexture layer geometry to render on
-//	floorTexture.rect(0,0,floorTexture.width,floorTexture.height);
+	floorTexture.rect(0,0,floorTexture.width,floorTexture.height);
 
 	background(255);
 	orbitControl(2,2,0);
