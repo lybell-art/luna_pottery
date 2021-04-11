92dfa56ef37ecbb5abecce6eb469558209650678
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-11 18:31:21 +0900

    Update lunapottery.js

--- lunapottery.js
+++ lunapottery.js
@@ -103,7 +103,7 @@ function draw()
 //	floorTexture.background(0);
 	floorTexture.fill(128);
 	// passing the shaderTexture layer geometry to render on
-	floorTexture.rect(0,0,floorTexture.width,floorTexture.height);
+	floorTexture.plain(1000);
 
 	background(255);
 	orbitControl(2,2,0);
